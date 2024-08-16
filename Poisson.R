2881196c84f06155721221a3804298df1d7e5a1a
salmanuin <147790165+salmanuin@users.noreply.github.com>
2024-08-16 17:44:49 +0700

    Regresi Poisson, BN & HBN.R

--- Poisson.R
+++ Poisson.R
@@ -1,42 +1,48 @@
-rm(list=ls())
-install.packages("MASS")
-install.packages("car")
-install.packages("pscl")
-install.packages("ggplot2")
-install.packages("boot")
-install.packages("AER")
-install.packages("fitdistrplus")
-install.packages("performance")
-install.packages("randomForest")
-install.packages("VGAM")
-install.packages("DT")
-install.packages("car")
-library(MASS)
+#Source Code Program R skripsi 2024 ANALISIS REGRESI POISSON, 
+#REGRESI BINOMIAL NEGATIF DAN REGRESI HURDLE BINOMIAL NEGATIF
+#PADA DATA OVERDISPERSI
+#(STUDI KASUS : JUMLAH KASUS KEMATIAN PENDERITA DEMAM BERDARAH
+# DENGUE DI PROVINSI JAWA TIMUR TAHUN 2022)
+#SALMAN AL-FARISI
+#19106010037
+#UIN SUNAN KALIJAGA YOGYAKARTA
 library(car)
-library(pscl)
-library(ggplot2)
-library(boot)
 library(AER)
-library(fitdistrplus)
-library(performance)
-library(randomForest)
-library(VGAM)
-library(DT) 
-library(car)
-data =read.delim("clipboard")
-str(data)
-model = lm(Y~., data = data)
+library(MASS)
+library(pscl)
+#1 Mengimpor data
+View(DBD2022)
+str(DBD2022)
+
+#2 Statistik deskriptif
+summary(DBD2022)
+var(DBD2022)
+
+#3 Uji distribusi Poisson
+ks.test(DBD2022$Y,"ppois", mean(DBD2022$Y))
+
+#4 Uji Multikolinearitas
+model = lm(Y~X1+X2+X3+X4+X5+X6+X7, data = DBD2022)
 vif(model)
 
-#Regresi Poisson
-m<- glm(Y~X1+X2+X3+X4+X5+X6, family="poisson", data=data)
+#5 Estimasi Parameter regresi Poisson
+m<- glm(Y~X1+X3+X4+X5+X6+X7, family="poisson", data=DBD2022)
 summary(m)
-check_overdispersion(m)
+pR2(m)
+qchisq(0.95, 6)
 
-#Regresi binomial negatif
-m2<- glm.nb(Y~X1+X2+X3+X4+X5+X6, data=data)
+#6 Uji overdispersi
+dispersiontest(m)
+
+#7 Estimasi parameter regresi Binomial Negatif
+m2<- glm.nb(Y~X1+X3+X4+X5+X6+X7, data=DBD2022)
 summary(m2)
+pR2(m2)
+qchisq(0.95, 6)
 
-#Membandingkan model
-model_performance(m,metrics = "all")
-model_performance(m2,metrics = "all")
+#8 Estimasi parameter regresi Hurdle Binomial Negatif
+m3<-hurdle(Y~X1+X3+X4+X5+X6+X7, data=DBD2022)
+summary(m3)
+pR2(m3)
+qchisq(0.95, 6)
+AIC(m3)
